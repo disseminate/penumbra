93b316848631db2b7090ef3196cb9f8abc83057c
mathlomaniac@rogers.com <mathlomaniac@rogers.com@7f937d04-7859-11de-8e60-65c8b4fa459e>
2009-07-27 12:29:30 +0000

    Weapons have been made permanent for more of a challenge.

--- trunk/gamemode/shared.lua
+++ trunk/gamemode/shared.lua
@@ -8,9 +8,9 @@ include( "shared_meta.lua" );
 
 BUY_ITEMS = {
 	
-	{ "Laser Pointer", "Your standard pointing tool, now a shadow-killing death ray.", 15, "weapon_laserpoint", "models/weapons/w_pistol.mdl" },
-	{ "Glowstick", "Creates an area-of-effect attack on shadows.", 30, "weapon_glowstick", "models/weapons/w_stunbaton.mdl" },
-	{ "Flashbang", "Special light-emitting grenade that causes a massive blast to shadows.", 40, "weapon_flashbang", "models/weapons/W_grenade.mdl" },
-	{ "Molotov Cocktail", "Think of it as a stationary turret, but cooler and with fire.", 60, "weapon_molotov", "models/props_junk/garbage_glassbottle003a.mdl" }
+	{ "Laser Pointer", "Your standard pointing tool, now a shadow-killing death ray.", 150, "weapon_laserpoint", "models/weapons/w_pistol.mdl", function( ply ) ply.HasLaserPointer = true end },
+	{ "Glowstick", "Creates an area-of-effect attack on shadows.", 300, "weapon_glowstick", "models/weapons/w_stunbaton.mdl", function( ply ) ply.HasGlowstick = true end },
+	{ "Flashbang", "Special light-emitting grenade that causes a massive blast to shadows.", 400, "weapon_flashbang", "models/weapons/W_grenade.mdl", function( ply ) ply.HasFlashbang = true end },
+	{ "Molotov Cocktail", "Think of it as a stationary turret, but cooler and with fire.", 600, "weapon_molotov", "models/props_junk/garbage_glassbottle003a.mdl", function( ply ) ply.HasMolotov = true end }
 	
 }
